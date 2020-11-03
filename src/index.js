da2df325ede74dc5b009f1beae1c1fd8972a0ed4
pashangshangpo <pashangshangpo@gmail.com>
2020-11-03 18:33:31 +0800

    chore: 代码风格

--- src/index.js
+++ src/index.js
@@ -1,81 +1,79 @@
 const path = require('path')
 const { readFilePaths, writeFile, exists } = require('fs-promise')
-const fileIcon = require("extract-file-icon")
+const fileIcon = require('extract-file-icon')
 
 const getAppFiles = async (appDirs, appExts) => {
-    const filesPromise = []
-    const fileKey = {}
+  const filesPromise = []
+  const fileKey = {}
 
-    for (let appDir of appDirs) {
-        filesPromise.push(
-            readFilePaths(appDir, filePath => {
-                return appExts.includes(path.extname(filePath))
-            })
-        )
+  for (let appDir of appDirs) {
+    filesPromise.push(
+      readFilePaths(appDir, filePath => {
+        return appExts.includes(path.extname(filePath))
+      })
+    )
+  }
+
+  const filesConcat = await Promise.all(filesPromise)
+
+  for (let section of filesConcat) {
+    for (let item of section) {
+      fileKey[item] = ''
     }
+  }
 
-    const filesConcat = await Promise.all(filesPromise)
+  const files = Object.keys(fileKey)
 
-    for (let section of filesConcat) {
-        for (let item of section) {
-            fileKey[item] = ''
-        }
-    }
-
-    const files = Object.keys(fileKey)
-
-    return files
+  return files
 }
 
 const getApps = async (appDirs, appExts) => {
-    const appFiles = await getAppFiles(appDirs, appExts)
-    const apps = []
-    const appIconsPromise = []
+  const appFiles = await getAppFiles(appDirs, appExts)
+  const apps = []
+  const appIconsPromise = []
 
-    for (let filePath of appFiles) {
-        const iconName = path.basename(filePath).replace(path.extname(filePath), '')
-        const appIconPath = path.resolve('.', 'apps', iconName + '.png')
+  for (let filePath of appFiles) {
+    const iconName = path.basename(filePath).replace(path.extname(filePath), '')
+    const appIconPath = path.resolve('.', 'apps', iconName + '.png')
 
-        apps.push({
-            name: iconName,
-            icon: appIconPath
-        })
+    apps.push({
+      name: iconName,
+      icon: appIconPath,
+    })
 
-        if (await exists(appIconPath)) {
-            continue
-        }
-
-        appIconsPromise.push(
-            writeFile(appIconPath, fileIcon(filePath, 32))
-        )
+    if (await exists(appIconPath)) {
+      continue
     }
 
-    await Promise.all(appIconsPromise)
+    appIconsPromise.push(writeFile(appIconPath, fileIcon(filePath, 32)))
+  }
 
-    return apps
+  await Promise.all(appIconsPromise)
+
+  return apps
 }
 
 export default (dirs = [], exts = []) => {
-    const appExts = [".appref-ms", ".exe", ".lnk", ".bat", ".url"]
-    const appDirs = [
-        'C:\\Users\\Default\\Links',
-        'C:\\Users\\Default\\Desktop',
-        'C:\\Users\\Administrator\\Desktop',
-        'C:\\Users\\Default\\AppData\\Roaming\\Microsoft\\Internet Explorer',
-        'C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs',
-    ]
+  const appExts = ['.appref-ms', '.exe', '.lnk', '.bat', '.url']
+  const appDirs = [
+    'C:\\Users\\Default\\Links',
+    'C:\\Users\\Default\\Desktop',
+    'C:\\Users\\Administrator\\Desktop',
+    'C:\\Users\\Default\\AppData\\Roaming\\Microsoft\\Internet Explorer',
+    'C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs',
+  ]
 
-    for (let dir of dirs) {
-        if (!appDirs.includes(dir)) {
-            appDirs.push(dir)
-        }
+  for (let dir of dirs) {
+    if (!appDirs.includes(dir)) {
+      appDirs.push(dir)
     }
+  }
 
-    for (let ext of exts) {
-        if (!appExts.includes(ext)) {
-            appExts.push(ext)
-        }
+  for (let ext of exts) {
+    if (!appExts.includes(ext)) {
+      appExts.push(ext)
     }
+  }
 
-    return getApps(appDirs, appExts)
+  return getApps(appDirs, appExts)
 }
